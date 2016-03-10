57d5c9092ab97cdeb3ee0a06ff992754f785d821
soasme <soasme@gmail.com>
2016-03-10 17:31:10 +0800

    Remove slack and git stuff.

--- fapistrano/deploy.py
+++ fapistrano/deploy.py
@@ -31,8 +31,6 @@ RELEASE_PATH_FORMAT = '%y%m%d-%H%M%S'
 env.show_output = False
 first_setup_repo_func = None
 setup_repo_func = None
-slack_sendbox = []
-slack_atexit = False
 
 def first_setup_repo(f):
     global first_setup_repo_func
@@ -44,129 +42,6 @@ def setup_repo(f):
     global setup_repo_func
     setup_repo_func = f
     return f
-
-
-def _get_config():
-    try:
-        with open(os.path.expanduser('~/.fapistranorc')) as f:
-            configs = yaml.load(f)
-            return configs.get(os.getcwd(), {})
-    except IOError:
-        return {}
-
-
-def _check_slack_sendbox(data):
-    if data in slack_sendbox:
-        return False
-    slack_sendbox.append(data)
-    return True
-
-
-def _send_to_slack(payload, **kw):
-    conf = _get_config()
-    if 'slack_webhook' not in conf:
-        return
-    if isinstance(payload, basestring):
-        payload = {'text': payload}
-    payload.setdefault('icon_emoji', ':trollface:')
-    payload.update(kw)
-    data = json.dumps(payload)
-    if not _check_slack_sendbox(data):
-        return
-
-    global slack_atexit
-    if not slack_atexit:
-        slack_atexit = True
-        @atexit.register
-        def send():
-            for data in slack_sendbox:
-                return requests.post(conf['slack_webhook'], data=data, timeout=10)
-
-
-def _format_target():
-    return '{app_name}-{env}'.format(**env)
-
-
-def _format_link(title, href):
-    return u'<%s|%s>' % (title, href)
-
-
-def _format_git_commit(commit):
-    conf = _get_config()
-    git_web = conf.get('git_web')
-    if not git_web:
-        return commit
-    return u'<%s%s|%s>' % (git_web, commit, commit)
-
-
-def _format_git_richlog(text):
-    if not text:
-        return
-
-    conf = _get_config()
-    git_web = conf.get('git_web')
-    if not git_web:
-        return
-    commits = []
-
-    for line in text.splitlines():
-        commit_hash, commit_log = line.split(' ', 1)
-        commits.append(u'<{git_web}{commit_hash}|{commit_hash}> {commit_log}'.format(**locals()))
-    return {
-        'value': u'\n'.join(commits) if commits else 'No commit.'
-    }
-
-
-def _format_delta_payload(delta_log):
-    notes = '[%s] Please check if the commits are ready to deploy.' % _format_target()
-    return _format_common_gitlog_payload(delta_log, notes, '#aaccaa')
-
-
-def _format_release_payload(delta_log):
-    notes = '[%s] Deploy to %s. Please check if it works properly.' % (
-        _format_target(), _format_git_commit(_get_remote_head())
-    )
-    return _format_common_gitlog_payload(delta_log, notes)
-
-
-def _format_common_gitlog_payload(gitlog, notes, color='#D00000'):
-    text = u'```%s```\n%s' % (gitlog if gitlog else 'No commit.', notes)
-
-    richlog = _format_git_richlog(gitlog)
-    if not richlog:
-        payload = { 'text': text }
-    else:
-        payload = {
-            'attachments': [
-                {
-                    'fallback': text,
-                    'color': color,
-                    'fields': [
-                        richlog,
-                        {
-                            'title': 'Notes',
-                            'value': notes
-                        },
-                    ],
-                },
-            ]
-        }
-
-    return payload
-
-
-def _get_remote_head():
-    with cd(env.current_path):
-        return run("git rev-parse --short HEAD", quiet=True)
-
-
-def _get_delta(upstream='upstream', bsd=True):
-    version = _get_remote_head()
-
-    local('git fetch -q %s' % upstream)
-    return local('git log --reverse --pretty="%%h %%s: %%b" --merges %s..%s/master | '
-                 'sed -%s "s/Merge pull request #([0-9]+) from ([^/]+)\\/[^:]+/#\\1\\/\\2/"' % (
-                     version, upstream, 'E' if bsd else 'r'), capture=True).decode('utf8')
 
 
 @task
@@ -290,45 +165,42 @@ def release(branch=None, refresh_supervisor=False, use_reset=False, bsd=True):
     if branch:
         env.branch = branch
 
-    delta_log = _get_delta(branch or 'upstream', bsd)
+    green_alert('Deploying new release on %(branch)s branch' % env)
 
-    if delta_log:
-        green_alert('Deploying new release on %(branch)s branch' % env)
+    # get releases
+    _releases()
 
-        # get releases
-        _releases()
+    green_alert('Creating the build path')
+    with cd(env.releases_path):
+        run('cp -rp %(current_release)s _build' % env)
 
-        green_alert('Creating the build path')
-        with cd(env.releases_path):
-            run('cp -rp %(current_release)s _build' % env)
+    try:
+        # update code and environments
+        with cd('%(releases_path)s/_build' % env):
+            signal('deploy.updating').send(
+                None,
+                git_user_reset=use_reset,
+                git_bsd=bsd,
+            )
 
-        try:
-            # update code and environments
-            with cd('%(releases_path)s/_build' % env):
-                signal('deploy.updating').send(
-                    None,
-                    git_user_reset=use_reset,
-                    git_bsd=bsd,
-                )
+            signal('deploy.updated').send(None)
 
-                signal('deploy.updated').send(None)
+            if callable(setup_repo_func):
+                # setup repo
+                green_alert('Setting up repo')
+                setup_repo_func()
 
-                if callable(setup_repo_func):
-                    # setup repo
-                    green_alert('Setting up repo')
-                    setup_repo_func()
+    except SystemExit:
+        red_alert('New release failed to build')
+        cleanup_failed()
+        exit()
 
-        except SystemExit:
-            red_alert('New release failed to build')
-            cleanup_failed()
-            exit()
-
-        green_alert('Symlinking to current')
-        with cd(env.releases_path):
-            run('mv _build %(new_release)s' % env)
-        with cd(env.path):
-            run('ln -nfs %(releases_path)s/%(new_release)s current' % env)
-        green_alert('Done. Deployed %(new_release)s on %(branch)s' % env)
+    green_alert('Symlinking to current')
+    with cd(env.releases_path):
+        run('mv _build %(new_release)s' % env)
+    with cd(env.path):
+        run('ln -nfs %(releases_path)s/%(new_release)s current' % env)
+    green_alert('Done. Deployed %(new_release)s on %(branch)s' % env)
 
     green_alert('Launching')
     restart(refresh_supervisor)
@@ -360,8 +232,6 @@ def rollback():
         run('ln -nfs %(releases_path)s/%(rollback_to)s current' % env)
         restart()
         run('rm -rf %(releases_path)s/%(rollback_from)s' % env)
-
-    green_alert('Rollback to %s' % _get_remote_head())
 
     signal('git.reverted').send(None)
 
