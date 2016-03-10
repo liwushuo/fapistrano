34808e53feb98a6a3d97c2c121743d8117c83c56
soasme <soasme@gmail.com>
2016-03-10 17:25:22 +0800

    Subscript deploy.reverted and deploy.updating.

--- fapistrano/plugins/git.py
+++ fapistrano/plugins/git.py
@@ -7,6 +7,8 @@ from ..utils import green_alert
 def init():
     signal('deploy.delta.publishing').connect(publish_git_delta)
     signal('deploy.head.publishing').connect(publish_git_head)
+    signal('deploy.reverted').connect(check_reverted_git_repo)
+    signal('deploy.updating').connect(update_git_repo)
 
 def publish_git_delta(sender=None, **kwargs):
     delta_log = _get_delta()
@@ -20,6 +22,29 @@ def publish_git_head(sender=None, **kwargs):
     head = _get_remote_head()
     green_alert('Get head: \n%s' % head)
     signal('git.head.publishing').send(None, head=head)
+
+def check_reverted_git_repo(sender=None, **kwargs):
+    head = _get_remote_head()
+    green_alert('Get head: \n%s' % head)
+    signal('git.reverted').send(None, head=head)
+
+def update_git_repo(sender=None, **kwargs):
+    delta_log = _get_delta(bsd=kwargs.get('git_bsd', True))
+
+    if kwargs.get('git_use_reset'):
+        run('git fetch -q')
+        run('git reset --hard origin/%(branch)s' % env)
+    else:
+        run('git pull -q')
+        run('git checkout %(branch)s' % env)
+
+    head = _get_remote_head()
+    green_alert('Release to %s' % head)
+
+    if delta_log:
+        green_alert('Release log:\n%s' % delta_log)
+
+    signal('git.updated').send(None, delta_log=delta_log, head=head)
 
 
 def _get_remote_head():
