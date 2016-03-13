7720a5200ce2cbb2405eabf85393fec09a4c0e55
soasme <soasme@gmail.com>
2016-03-13 17:02:31 +0800

    Refactor release task.

--- fapistrano/deploy.py
+++ fapistrano/deploy.py
@@ -103,99 +103,53 @@ def cleanup_rollback():
 
 @task
 @with_configs
-def setup(branch=None):
-    if branch:
-        env.branch = branch
 def cleanup():
     green_alert('Cleanning up old release(s)')
     with cd(get_releases_path()):
         run('rm -rf %s' % ' '.join(get_outdated_releases()))
 
-    green_alert('Creating project path')
+def _check():
     run('mkdir -p %(path)s/{releases,shared/log}' % env)
-
-    # change permission
-    run('find %(path)s -type d -exec chmod 755 {} \;' % env)
-    run('find %(path)s -type f -exec chmod 644 {} \;' % env)
     run('chmod -R g+w %(shared_path)s' % env)
 
-    # clone code
-    with cd(env.releases_path):
-        green_alert('Cloning the latest code')
-        env.new_release = datetime.now().strftime(RELEASE_PATH_FORMAT)
-        run('git clone -q --depth 1 %(repo)s _build' % env)
-
-    with cd('%(releases_path)s/_build' % env):
-        green_alert('Checking out %(branch)s branch' % env)
-        run('git checkout %(branch)s' % env)
-
-        if callable(first_setup_repo_func):
-            green_alert('Setting up repo')
-            first_setup_repo_func()
-
-    # symlink
-    with cd(env.releases_path):
-        run('mv _build %(new_release)s' % env)
+def _symlink_current(dest):
+    green_alert('Symlinking to current')
     with cd(env.path):
-        run('ln -nfs %(releases_path)s/%(new_release)s current' % env)
+        run('ln -nfs %s current' % dest)
+    green_alert('Done. Deployed %s' % dest)
 
-        # link supervisorctl and run
-        run('ln -nfs %(current_path)s/configs/supervisor_%(env)s_%(role)s.conf '
-            '/etc/supervisor/conf.d/%(project_name)s.conf' % env)
-        run('supervisorctl reread')
-        run('supervisorctl update')
-        run('supervisorctl status')
+def _symlink_new_release():
+    _symlink_current('%(releases_path)s/%(new_release)s' % env)
 
 def _symlink_rollback():
     _symlink_current('%(releases_path)s/%(rollback_to)s' % env)
 
 @task
 @with_configs
-def release(branch=None, refresh_supervisor=False, use_reset=False, bsd=True):
-    if branch:
-        env.branch = branch
+def release(branch=None):
+    env.branch = branch if branch else env.branch
+    env.new_release = datetime.now().strftime(RELEASE_PATH_FORMAT)
 
     green_alert('Deploying new release on %(branch)s branch' % env)
 
-    # get releases
-    _releases()
+    green_alert('Starting')
+    signal('deploy.starting').send(None)
+    _check()
+    signal('deploy.started').send(None)
 
-    green_alert('Creating the build path')
-    with cd(env.releases_path):
-        run('cp -rp %(current_release)s _build' % env)
+    green_alert('Updating')
+    signal('deploy.updating').send(None)
+    signal('deploy.updated').send(None)
 
-    try:
-        # update code and environments
-        with cd('%(releases_path)s/_build' % env):
-            signal('deploy.updating').send(
-                None,
-                git_user_reset=use_reset,
-                git_bsd=bsd,
-            )
+    green_alert('Publishing')
+    signal('deploy.publishing').send(None)
+    _symlink_new_release()
+    signal('deploy.published').send(None)
 
-            signal('deploy.updated').send(None)
-
-            if callable(setup_repo_func):
-                # setup repo
-                green_alert('Setting up repo')
-                setup_repo_func()
-
-    except SystemExit:
-        red_alert('New release failed to build')
-        cleanup_failed()
-        exit()
-
-    green_alert('Symlinking to current')
-    with cd(env.releases_path):
-        run('mv _build %(new_release)s' % env)
-    with cd(env.path):
-        run('ln -nfs %(releases_path)s/%(new_release)s current' % env)
-    green_alert('Done. Deployed %(new_release)s on %(branch)s' % env)
-
-    green_alert('Launching')
-    restart(refresh_supervisor)
-
+    green_alert('Finishing')
+    signal('deploy.finishing').send(None)
     cleanup()
+    signal('deploy.finished').send(None)
 
     # TODO: do rollback when restart failed
 
