7e66df18f71c03267db4845ee36c5a711c64266b
soasme <soasme@gmail.com>
2016-03-14 17:30:09 +0800

    Init nothing if there is no plugin.

--- fapistrano/app.py
+++ fapistrano/app.py
@@ -5,6 +5,8 @@ from importlib import import_module
 from fabric.api import env
 
 def init():
+    if not hasattr(env, 'plugins'):
+        return
     for plugin in env.plugins:
         mod = import_module(plugin)
         mod.init()
