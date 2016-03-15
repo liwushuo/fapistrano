7e265e76d0351eb4e1fb2ac35c7de3f89ad73a78
soasme <soasme@gmail.com>
2016-03-15 14:12:19 +0800

    Apply fis configurations.

--- fapistrano/plugins/fis.py
+++ fapistrano/plugins/fis.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import show, run, env, cd
+from fabric.api import show, run, env, cd, show, hide
 
-from .. import signal
+from .. import signal, configuration
 
 def init():
+    configuration.setdefault('fis_output', False)
+    configuration.setdefault('fis_domain', False)
+    configuration.setdefault('fis_optimize', True)
+    configuration.setdefault('fis_pack', True)
     signal.register('deploy.updated', build_fis_assets)
 
 def build_fis_assets():
-    with show('output'):
-        run('''
-        fis release --file  %(releases_path)s/%(new_release)s/%(fis_conf)s \
-        --dest  %(releases_path)s/%(new_release)s/%(fis_dest)s \
-        --root %(releases_path)s/%(new_release)s/%(fis_source)s \
-        --optimize \
-        --pack \
-        --md5
-        ''' % env)
+    output = show if env.fis_output else hide
+    with output('output'):
+        cmd = ('fis release --file %(release_path)s/%(fis_conf)s '
+            '--dest %(release_path)s/%(fis_dest)s '
+            '--root %(release_path)s/%(fis_source)s ') % env
+        if env.fis_optimize:
+            cmd += '--optimize '
+        if env.fis_pack:
+            cmd += '--pack '
+        if env.fis_domain:
+            cmd += '--domain '
+        run(cmd)
