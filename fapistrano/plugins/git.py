f5e12900aae570676be0502853d1d8f9e245d851
soasme <soasme@gmail.com>
2016-03-15 10:37:49 +0800

    Refactor git revision.

--- fapistrano/plugins/git.py
+++ fapistrano/plugins/git.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import cd, env, run, local, task
+from fabric.api import cd, env, run, local, task, abort
 from fabric.contrib.files import exists
 from ..utils import green_alert, red_alert, yellow_alert
 from .. import signal
@@ -18,38 +18,29 @@ def init():
     if not hasattr(env, 'branch'):
         env.branch = 'master'
 
-    signal.register('deploy.delta.publishing', publish_git_delta)
-    signal.register('deploy.head.publishing', publish_git_head)
-
-    signal.register('deploy.reverted', log_reverted_revision)
     signal.register('deploy.started', check_repo)
+    signal.register('deploy.reverting', log_previous_revision)
+    signal.register('deploy.finishing_rollback', log_rollback_revision)
     signal.register('deploy.updating', update_git_repo)
 
-def publish_git_delta(**kwargs):
-    head = _get_remote_head()
-    delta_log = _get_delta(head)
-    if not delta_log:
-        green_alert('No delta.')
-        return
-    green_alert('Get delta:\n%s' % delta_log)
-    signal.emit('git.delta.publishing', head=head, delta_log=delta_log)
 def check_repo(**kwargs):
     _check()
 
-def publish_git_head(**kwargs):
-    head = _get_remote_head()
-    green_alert('Get head: \n%s' % head)
-    signal.emit('git.head.publishing', head=head)
+def log_previous_revision(**kwargs):
+    head = _read_current_revision()
+    green_alert('Rollback from %s' % head)
 
-def log_reverted_revision(**kwargs):
-    head = _get_remote_head()
+def log_rollback_revision(**kwargs):
+    head = _read_current_revision()
     green_alert('Rollback to %s' % head)
-    signal.emit('git.reverted', head=head)
 
 def update_git_repo(**kwargs):
     if not exists('%(path)s/repo' % env):
         _clone()
     _update()
+    _set_current_version()
+    _release()
+    _echo_revision()
 
 
 def _get_remote_head():
@@ -105,3 +96,33 @@ def _update():
             )
         else:
             run('git remote update --prune')
+
+
+def _release():
+    with cd('%(path)s/repo' % env):
+        run('git archive %(branch)s '
+            '| tar -x -f - -C %(releases_path)s/%(new_release)s' % env)
+
+
+def _get_revision():
+    with cd('%(path)s/repo' % env):
+        return run(
+            'git rev-list '
+            '--max-count=1 '
+            '--abbrev-commit '
+            '--abbrev=12 '
+            '%(branch)s' % env
+        )
+
+
+def _echo_revision():
+    with cd('%(releases_path)s/%(new_release)s' % env):
+        run('echo %(current_version)s >> REVISION' % env)
+
+
+def _read_current_revision():
+    return run('cat %(current_path)s/REVISION' % env)
+
+
+def _set_current_version():
+    env.current_version = _get_revision()
