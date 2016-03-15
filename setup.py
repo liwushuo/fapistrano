348a276a2c1fb5a2351f1feaa86c12bfb95154a3
soasme <soasme@gmail.com>
2016-03-15 13:24:05 +0800

    Add open source files.

--- setup.py
+++ setup.py
@@ -15,31 +15,45 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'Fabric',
+    'requests',
+    'PyYaml',
+    'click',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
 ]
 
 setup(
     name='fapistrano',
-    version='0.5.1',
+    version='0.6.0',
     license='MIT',
-    description='Capistrano style deployment with fabric',
+    description="A remote server automation and deployment tool.",
+    long_description=readme + '\n\n' + history,
     zip_safe=False,
     include_package_data=True,
+    install_requires=requirements,
     platforms='any',
+    author="Ju Lin",
+    author_email='soasme@gmail.com',
+    url='https://github.com/soasme/fapistrano',
     packages=['fapistrano'],
-    install_requires=[
-        'Fabric',
-        'requests',
-        'PyYaml',
-    ],
-    entry_points='''
-        [console_scripts]
-        fap=fapistrano.cli:fap
-    '''
+    package_dir={'fapistrano': 'fapistrano'},
+    entry_points="""
+    [console_scripts]
+    fap=fapistrano.cli:fap
+    """,
+    keywords='fapistrano, deploy, deployment, automate, automation, fabric, remote, production, staging, development',
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-    ]
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
