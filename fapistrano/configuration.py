e3e5bfcafc80ea1f9881ce08c120691a57f2a6e7
soasme <soasme@gmail.com>
2016-03-15 18:07:29 +0800

    Move methods from utils to configuration.

--- fapistrano/configuration.py
+++ fapistrano/configuration.py
@@ -38,12 +38,57 @@ def setdefault(key, value, force=False):
     elif not hasattr(env, key):
         setattr(env, key, value)
 
+RELEASE_PATH_FORMAT = '%y%m%d-%H%M%S'
+
 def set_default_configurations():
     setdefault('show_output', False)
     setdefault('user', 'deploy')
     setdefault('use_ssh_config', True)
-    setdefault('path', '/home/%(user)s/www/%(project_name)s')
+    setdefault('path', '/home/%(user)s/www/%(project_name)s', force=True)
+    setdefault('current_path', '%(path)s/current')
+    setdefault('releases_path', '%(path)s/releases')
+    setdefault('shared_path', '%(path)s/shared')
+    setdefault('new_release', datetime.now().strftime(RELEASE_PATH_FORMAT))
+    setdefault('release_path', '%(releases_path)s/%(new_release)s')
     setdefault('linked_files', [])
     setdefault('linked_dirs', [])
     setdefault('env_role_configs', {})
     setdefault('keey_releases', 5)
+
+def check_stage_and_role():
+    stage = env.get('stage')
+    role = env.get('role')
+
+    # raise error when env/role not set both
+    if not stage or not role:
+        abort('stage or role not set!')
+
+def apply_configurations_to_env(conf):
+    for env_item in conf:
+        env_value = conf.get(env_item)
+        setattr(env, env_item, env_value)
+
+def apply_role_configurations_to_env(stage, role):
+    if stage in env.stage_role_configs:
+        if role in env.stage_role_configs[stage]:
+            config = env.stage_role_configs[stage][role]
+            apply_configurations_to_env(config)
+
+
+def apply_env(stage, role):
+    env.stage = stage
+    env.role = role
+    check_stage_and_role()
+    set_default_configurations()
+    apply_role_configurations_to_env(stage, role)
+    apply_configurations_to_env(format_definition())
+
+
+def with_configs(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        output_func = show if env.show_output else hide
+        with output_func('output'):
+            ret = func(*args, **kwargs)
+        return ret
+    return wrapped
--- fapistrano/utils.py
+++ fapistrano/utils.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-from functools import wraps
-
-from fabric.api import env, show, hide, abort
 from fabric.colors import green, red, white, yellow
-from . import configuration
 
-RELEASE_PATH_FORMAT = '%y%m%d-%H%M%S'
 
 def red_alert(msg, bold=True):
     print red('===>', bold=bold), white(msg, bold=bold)
@@ -19,87 +13,3 @@ def green_alert(msg, bold=True):
 
 def yellow_alert(msg, bold=True):
     print yellow('===>', bold=bold), white(msg, bold=bold)
-
-
-def _apply_checking():
-    stage = env.get('env')
-    role = env.get('role')
-
-    # raise error when env/role not set both
-    if not stage or not role:
-        abort('env or role not set!')
-
-
-# hosts config must be set before task running
-
-def _get_supervisor_target(project_name, as_group=False):
-    return '%s:*' % project_name if as_group else project_name
-
-def _parse_env_role_config(env, config):
-    project_name = config.get('project_name', env.project_name)
-    as_group = bool(config.get('as_group'))
-    config.setdefault('supervisor_target', _get_supervisor_target(project_name, as_group))
-    return config
-
-def _apply_env_role_config():
-    stage = env.get('env')
-    role = env.get('role')
-
-    # ensure stage and role are set
-    if not stage or not role:
-        return
-
-    if stage in env.env_role_configs:
-        if role in env.env_role_configs[stage]:
-            config = env.env_role_configs[stage][role]
-            config = _parse_env_role_config(env, config)
-            env.update(config)
-
-    env.path = '/home/%(user)s/www/%(project_name)s' % env
-    env.current_path = '%(path)s/current' % env
-    env.releases_path = '%(path)s/releases' % env
-    env.shared_path = '%(path)s/shared' % env
-    env.activate = 'source ~/.virtualenvs/%(project_name)s/bin/activate' % env
-    env.new_release = datetime.now().strftime(RELEASE_PATH_FORMAT)
-    env.release_path = '%(releases_path)s/%(new_release)s' % env
-
-def _apply_env_to_configurations():
-    for env_item in env:
-        if not isinstance(getattr(env, env_item), str):
-            continue
-        env_value = configuration.format(getattr(env, env_item))
-        setattr(env, env_item, env_value)
-
-def with_configs(func):
-    @wraps(func)
-    def wrapped(*args, **kwargs):
-        configuration.set_default_configurations()
-        _apply_checking()
-        _apply_env_to_configurations()
-        output_func = show if env.show_output else hide
-        with output_func('output'):
-            ret = func(*args, **kwargs)
-        return ret
-    return wrapped
-
-
-def register_role(role):
-    def wrapper(func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            env.role = role
-            _apply_env_role_config()
-            return func(*args, **kwargs)
-        return wrapped
-    return wrapper
-
-
-def register_env(stage):
-    def wrapper(func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            env.env = stage
-            _apply_env_role_config()
-            return func(*args, **kwargs)
-        return wrapped
-    return wrapper
