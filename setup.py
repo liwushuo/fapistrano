b2ef0fb1ac746d9c0d958fb343b403f3e774c9e4
soasme <soasme@gmail.com>
2016-03-15 11:34:12 +0800

    Add read and history read in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
 
 setup(
     name='fapistrano',
