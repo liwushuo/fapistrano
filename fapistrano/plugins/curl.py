87bdef439a3faf465bb8c23166beeb8a142400f7
soasme <soasme@gmail.com>
2016-03-18 12:40:38 +0800

    Add extract_tar and post_install_script option.

--- fapistrano/plugins/curl.py
+++ fapistrano/plugins/curl.py
@@ -6,8 +6,15 @@ from .. import signal, configuration
 def init():
     configuration.setdefault('curl_url', '')
     configuration.setdefault('curl_options', '')
+    configuration.setdefault('curl_extract_tar', '')
+    configuration.setdefault('curl_postinstall_script', '')
     signal.register('deploy.updating', download_artifact)
 
 def download_artifact(**kwargs):
     with cd(env.release_path):
-        run('curl %(curl_url)s %(curl_options)s' % env)
+        cmd = 'curl %(curl_url)s %(curl_options)s' % env
+        if env.curl_extract_tar:
+            cmd += ' | tar -x'
+        run(cmd)
+        if env.curl_postinstall_script:
+            run(env.curl_postinstall_script)
