6575b75c76a2d4a2431495a267f6da644a2784d4
soasme <soasme@gmail.com>
2016-03-18 12:41:12 +0800

    Extract method: _setup_execution.

--- fapistrano/cli.py
+++ fapistrano/cli.py
@@ -6,9 +6,48 @@ import yaml
 from os import environ
 from click.parser import OptionParser
 from fabric.api import env, execute
-from fapistrano.app import init_cli
-from fapistrano.configuration import apply_env
+from fapistrano.configuration import apply_env, apply_yaml_to_env
+from fapistrano import app
 from fapistrano import deploy
+
+
+@click.group()
+@click.option('-d', '--deployfile', default='./deploy.yml')
+def fap(deployfile):
+    with open(deployfile, 'rb') as f:
+        apply_yaml_to_env(f.read())
+
+
+@fap.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
+def release(role, stage, plugin_args):
+    _setup_execution(role, stage, plugin_args)
+    execute(deploy.release)
+
+@fap.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
+def rollback(role, stage, plugin_args):
+    _setup_execution(role, stage, plugin_args)
+    execute(deploy.rollback)
+
+@fap.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
+def restart(role, stage, plugin_args):
+    _setup_execution(role, stage, plugin_args)
+    execute(deploy.restart)
+
 
 def _apply_plugin_options(plugin_args):
     parser = OptionParser()
@@ -20,51 +59,11 @@ def _apply_plugin_options(plugin_args):
     for arg_key in order:
         setattr(env, arg_key, opts[arg_key])
 
-@click.group()
-@click.option('-d', '--deployfile', default='./deploy.yml')
-def fap(deployfile):
-    with open(deployfile, 'rb') as f:
-        conf = yaml.load(f.read())
-        init_cli(conf)
-
-@fap.command(context_settings=dict(
-    ignore_unknown_options=True,
-))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging', allow_from_autoenv=True)
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron', allow_from_autoenv=True)
-@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
-def release(role, stage, plugin_args):
+def _setup_execution(role, stage, plugin_args):
     env.role = role
     env.stage = stage
     apply_env(stage, role)
     _apply_plugin_options(plugin_args)
-    execute(deploy.release)
-
-@fap.command(context_settings=dict(
-    ignore_unknown_options=True,
-))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
-@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
-def rollback(role, stage, plugin_args):
-    env.role = role
-    env.stage = stage
-    apply_env(stage, role)
-    _apply_plugin_options(plugin_args)
-    execute(deploy.rollback)
-
-@fap.command(context_settings=dict(
-    ignore_unknown_options=True,
-))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
-@click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
-def restart(role, stage, plugin_args):
-    env.role = role
-    env.stage = stage
-    apply_env(stage, role)
-    _apply_plugin_options(plugin_args)
-    execute(deploy.restart)
 
 if __name__ == '__main__':
     import os
