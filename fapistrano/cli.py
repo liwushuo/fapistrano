b6e1b04011a8164a839c9616ee417d2f152331e7
soasme <soasme@gmail.com>
2016-03-28 10:03:15 +0800

    Support running by stage or role.

--- fapistrano/cli.py
+++ fapistrano/cli.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
 
 import click
-import yaml
-from os import environ
 from click.parser import OptionParser
 from fabric.api import env, execute
+
 from fapistrano.configuration import apply_env, apply_yaml_to_env
-from fapistrano import app
 from fapistrano import deploy
 
 
@@ -18,43 +17,40 @@ def fap(deployfile):
         with open(deployfile, 'rb') as f:
             apply_yaml_to_env(f.read())
     except IOError:
-        if deploy_file == './deploy.yml':
-            print("cannot find deployfile. Did you put a deploy.yml file on current directory?")
-            exit(1)
+        if deployfile == './deploy.yml':
+            _abort("cannot find deployfile. Did you put a deploy.yml file on current directory?")
         else:
-            print('cannot find deployfile. Does this file really exist?')
-            exit(1)
+            _abort('cannot find deployfile. Does this file really exist?')
 
 
 @fap.command(context_settings=dict(
     ignore_unknown_options=True,
 ))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.option('-r', '--role', help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', help='deploy stage, for example: app, worker, cron')
 @click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
 def release(role, stage, plugin_args):
-    _setup_execution(role, stage, plugin_args)
-    execute(deploy.release)
+    _execute(deploy.release, stage, role, plugin_args)
+
 
 @fap.command(context_settings=dict(
     ignore_unknown_options=True,
 ))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.option('-r', '--role', help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', help='deploy stage, for example: app, worker, cron')
 @click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
 def rollback(role, stage, plugin_args):
-    _setup_execution(role, stage, plugin_args)
-    execute(deploy.rollback)
+    _execute(deploy.rollback, stage, role, plugin_args)
+
 
 @fap.command(context_settings=dict(
     ignore_unknown_options=True,
 ))
-@click.option('-r', '--role', required=True, help='deploy role, for example: production, staging')
-@click.option('-s', '--stage', required=True, help='deploy stage, for example: app, worker, cron')
+@click.option('-r', '--role', help='deploy role, for example: production, staging')
+@click.option('-s', '--stage', help='deploy stage, for example: app, worker, cron')
 @click.argument('plugin_args', nargs=-1, type=click.UNPROCESSED)
 def restart(role, stage, plugin_args):
-    _setup_execution(role, stage, plugin_args)
-    execute(deploy.restart)
+    _execute(deploy.restart, stage, role, plugin_args)
 
 
 def _apply_plugin_options(plugin_args):
@@ -72,6 +68,44 @@ def _setup_execution(role, stage, plugin_args):
     env.stage = stage
     apply_env(stage, role)
     _apply_plugin_options(plugin_args)
+
+def _abort(message):
+    click.secho(message, blink=True, fg='red')
+    exit(1)
+
+def _get_execute_stage_and_roles(stage, role):
+    if not stage and not role:
+        _abort('Stage or role not found.')
+    elif not role and stage not in env.stage_role_configs:
+        _abort('Stage not found.')
+    elif not role and not env.stage_role_configs[stage]:
+        _abort('No role defined in this stage.')
+    elif not role:
+        _stage = env.stage_role_configs[stage]
+        for _role in _stage.keys():
+            yield (stage, _role)
+        return
+
+    roles = defaultdict(set)
+    for _stage in env.stage_role_configs:
+        for _role in env.stage_role_configs[_stage]:
+            roles[_role].add(_stage)
+
+    if role not in roles:
+        _abort('Role not found.')
+    elif not roles[role]:
+        _abort('No stage defined for this role.')
+    else:
+        for stage in roles[role]:
+            yield (stage, role)
+
+def _execute(method, stage=None, role=None, plugin_args=None):
+    combinations = _get_execute_stage_and_roles(stage, role)
+    for stage, role in combinations:
+        print stage, role
+        _setup_execution(role, stage, plugin_args)
+        execute(method)
+
 
 if __name__ == '__main__':
     import os
