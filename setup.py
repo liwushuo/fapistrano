a44e464fb9ca389be0b53bafd74667b3b6a44c89
soasme <soasme@gmail.com>
2016-04-09 13:07:08 +0800

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='fapistrano',
-    version='0.8.1',
+    version='0.8.2',
     license='MIT',
     description="A remote server automation and deployment tool.",
     long_description=readme + '\n\n' + history,
